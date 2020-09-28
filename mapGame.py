710ee6c994c0d8a60822cf65197fc7b3a2c8d138
Joel-Co <colaso.joel@gmail.com>
2020-09-27 22:24:58 -0300

    Changed how octaves are used for making the noise list. Before, it was the same for all OpenSimplexNoise objects, now it works as an argument given when creating the noise object

--- mapGame.py
+++ mapGame.py
@@ -1,8 +1,12 @@
 '''
 
 This program is meant to store the game.
+Currently it creates the folders for images and seeds and
+creates the map
 
 '''
+
+#We import the modules we are going to use.
 import os
 import pygame
 from opensimplex import OpenSimplex
@@ -10,26 +14,33 @@ import numpy as np
 import random
 from PIL import Image
 
+#We import the classes from our noiseGenerator and mapGenerator files.
 from noiseGenerator import *
 from mapGenerator import *
 
+
 def createDirs(*names):
+    '''This function takes an arbitrary number of strings and creates
+    directories with those names if they do not exist'''
+    
     for name in names:
         try:
             os.mkdir(name)
         except:
             pass
 
-createDirs("gameImages", "seeds")
-
-
-
-
 def createMap():
-    gameMap = Map("mapTest", 500, 500, 3)
+    '''Function to generate the map we'll be using in the game'''
+    gameMap = Map("gameMap", 500, 500, 3)
     gameMap.generateNoiseArray()
     gameMap.mapArbValueToColorValue()
     gameMap.createAndSaveMapAsPNG('gameImages\ ')
     gameMap.saveSeedAsTxtFile('seeds\ ')
 
+
+#We execute the map-creating function.
 createMap()
+
+#Create the folders we will use to store the map(s) and seed(s).
+createDirs("gameImages", "seeds")
+
