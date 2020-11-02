dbc4404e68b0a4be687de22a401dba849d455a90
Nicolas-ISN <70545104+Nicolas-ISN@users.noreply.github.com>
2020-11-02 19:23:06 -0300

    Update mapGame.py
    hola

--- mapGame.py
+++ mapGame.py
@@ -8,6 +8,7 @@ creates the map
 
 #We import the modules we are going to use.
 import os
+import sys
 import pygame
 from opensimplex import OpenSimplex
 import numpy as np
@@ -45,11 +46,107 @@ def createMap(name, width, height, freq, octaves, seed = None):
     gameMap.saveAsPNG('gameImages\ ')
     gameMap.createAndSaveMapAsPNG('gameImages\ ')
     gameMap.saveSeedAsTxtFile('seeds\ ')
+
+def rightGo():
+    global Xvis
+    Xvis = Xvis -1
+    screen.blit(MUNDI, [Xvis, Yvis])
+    #print ("funciona1")
+
+def leftGo():
+    global Xvis
+    Xvis = Xvis +1
+    screen.blit(MUNDI, [Xvis, Yvis])
+    #print ("funciona2")
+
+def downGo():
+    global Yvis
+    Yvis = Yvis -1
+    screen.blit(MUNDI, [Xvis, Yvis])
+    #print ("funciona3")
+
+def upGo():
+    global Yvis
+    Yvis = Yvis +1
+    screen.blit(MUNDI, [Xvis, Yvis])
+    #print ("funciona4")
+
+
+def downLimit():
+    global Yvis
+    global Ysize
+    Yvis = Ysize
+    screen.blit(MUNDI, [Xvis, Yvis])
+
+def upLimit():
+    global Yvis
+    Yvis = 0
+    screen.blit(MUNDI, [Xvis, Yvis])
+
+def leftLimit():
+    global Xvis
+    Xvis = 0
+    screen.blit(MUNDI, [Xvis, Yvis])
+
+def rightLimit():
+    global Xvis
+    global Xsize
+    Xvis = Xsize
+    screen.blit(MUNDI, [Xvis, Yvis])
+    
+pygame.init()   
+
+Xvis = 0 
+Yvis = 0 
+Xsize = 500
+Ysize = 500
+Xmundi = 1000
+Ymundi = 1000
+size = Xsize, Ysize 
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("EXPLORER")
+
+MUNDI0 = pygame.image.load("gameMap_map.png").convert()       
+MUNDI = pygame.transform.scale(MUNDI0,(Xmundi,Ymundi))
+
+run=True
+
+while run:
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: run = False
+        
+    Mouse_x, Mouse_y = pygame.mouse.get_pos()
+    print (str(Mouse_x), str(Mouse_y))
+    if Mouse_x >= (Xsize) -100:
+        rightGo()
+    if Mouse_x <= 100:
+        leftGo()
+    if Mouse_y >= (Xsize) -100:
+        downGo()
+    if Mouse_y <= 100:
+        upGo()
+    if Yvis >= 0:
+        upLimit()
+    if (Yvis) + (Xmundi) <= Ysize:
+        downLimit()
+    if Xvis >= 0:
+        leftLimit()
+    if (Xvis) + (Xmundi) <= Xsize:
+        rightLimit()
+
+    screen.blit(MUNDI, [Xvis, Yvis])
+    pygame.display.flip()                                  
+
+
+pygame.quit()
+
+
     
 #Create the folders we will use to store the map(s) and seed(s).
 createDirs("gameImages", "seeds")
 
 #We execute the map-creating function.
 createMap("gameMap", 500, 500, 5, octaves)
+
 
 
